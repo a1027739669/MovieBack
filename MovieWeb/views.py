842562c4a3bec89c4540e3877870f79127a0d6f3
Komorebi <1027739669@qq.com>
2021-03-07 14:02:42 +0800

    基于物品的推荐修改

--- MovieWeb/views.py
+++ MovieWeb/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.core import serializers
 import json
+from program import itemrec
+import multiprocessing
+file_path="data/rating.csv"
+itemBasedCF = itemrec.ItemBasedCF()
+itemBasedCF.load_data(file_path)
+itemBasedCF.data_transfer()
+itemBasedCF.similarity()
 from .models import *
 # Create your views here.
 
-def hello(request):
-    pass
+def iter_recommend(request):
+    userid=request.get('userid')
+    print(userid)
+    # result=itemBasedCF.recommendation(userid)
+    return JsonResponse("hello")
